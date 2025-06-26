477c6a9850112cc242bac642e349a46ed88fae80
Abhinav Shinde <143414991+Abhi9shinde@users.noreply.github.com>
2025-06-26 10:42:16 +0530

    no-sandbox

--- bookScrape.js
+++ bookScrape.js
@@ -1,7 +1,10 @@
 const puppeteer = require("puppeteer");
 
 async function getBooks(topic) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.goto("https://books.google.com/");
   await page.type('input[name="q"]', topic);
